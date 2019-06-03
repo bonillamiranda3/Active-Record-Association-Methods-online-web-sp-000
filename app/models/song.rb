c4167c39d346aa5db53f8a41ff87d7524623c74d
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-06-03 02:45:13 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,11 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    self.songs.count
   end
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
+    self.songs.map { |song| song.artist.name }
   end
 end
